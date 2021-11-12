0679721d254606b238389f56b63c4bdaf991ecf8
Chance Sturrup <chance@chancesturrup.nomads.utk.edu>
2021-11-12 11:44:30 -0500

    Homework 9

--- app.R
+++ app.R
@@ -12,36 +12,57 @@ library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
     # Application title
-    titlePanel("DiamondsShiny"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Diamonds organized by Color"),
+    
+    # Sidebar with a slider input for the specific Diamond Color
     sidebarLayout(
         sidebarPanel(
-            selectInput(inputId = "color", label = "Color",
-                        c("All",
+            selectInput(inputId = "color", label = "color",
+                        c(
                           unique(as.character(diamonds$color))
                         ),
-            tableOutput("diamonds")
+                        tableOutput("diamonds")
             )
         ),
-    
-
-        # Show a plot of the generated distribution
+        
+        
+        # Showing two plots of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            plotOutput("distPlot"),
+            plotOutput("distplot2")
         )
     )
 )
 
-    
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    # Generating a plot for all colors displayed simultaneously  
     output$distPlot <- renderPlot({
-        diamonds.color <- group_by(diamonds, color)
-    ggplot(data=diamonds.color, aes_string(x = "carat", y = "price"))    
+    
+        # Creating the plot and defining the desired aesthetics for all colors
+        ggplot(diamonds, aes_string(x = "carat", y = "price", color = "color"))+
+            geom_point()+
+            ggtitle("Relationship between Carat and Price")+ theme(
+                plot.title = element_text(color="red", size=20, face="bold.italic")
+            )
+    })
+    
+    # Organizing the data to allow each specific color to be displayed individually rather than as a bulk group.
+    output$distplot2 <- renderPlot({
+                diamonds <- diamonds[diamonds$color == input$color,]
+            
+            
+            # Creating the plot and defining the desired aesthetics 
+            ggplot(diamonds, aes_string(x = "carat", y = "price"))+
+                geom_point()+
+                ggtitle("Relationship between Carat and Price per Color", input$color)+ theme(
+                    plot.title = element_text(color="red", size=20, face="bold.italic")
+                ) 
     })
 }
 
